f547a6c1ea03466c76559b427759ef046793e104
Andrew Renninger <awrenninger@crimson.ua.edu>
2022-08-25 14:44:51 -0500

    start data store in mongo

--- ticketprice.py
+++ ticketprice.py
@@ -3,8 +3,28 @@ import sys
 import os
 from bs4 import BeautifulSoup
 from pandas import DataFrame
+from pymongo import MongoClient
+from pymongo.errors import ConnectionFailure
+from datetime import datetime
 
 STUDENT_SEATS_SITE="https://studentseats.com"
+MONGO_URL="mongodb://localhost:27017"
+MONGO_DATABASE="ticket"
+
+mongo_client=None
+mongo_db=None 
+mongo_col=None 
+
+def connectDb():
+    if mongo_client == None:
+        mongo_client=MongoClient(MONGO_URL)
+    try:
+        mongo_client.admin.command('ping')
+    except ConnectionFailure:
+        print("Database not availible")
+        mongo_client=None
+        return
+    mongo_db=mongo_client[MONGO_DATABASE]
 
 def getSite():
     os.system('cls' if os.name == 'nt' else 'clear')
@@ -53,13 +73,18 @@ if __name__ == "__main__":
         inst_out='\nNo arguments, reverting to default settings (lowerbowl, price point 70)\n\nUsage:  uaticketbot.exe (MAX price (integer)) ("upper" for upperbowl, anything else for lower)\nEx. uaticketbot.exe 100 lower\n\tuaticketbot.exe 60 upper stop_on_find\n\nCTRL+C to stop\n\n'
         standard_out+=inst_out
         #default stats
-        pArgs=[":)",str(70),"lower"]
+        pArgs=[":",str(70),"lower"]
     
     ticket_request_url = getSite()
     maxPrice= pArgs[1]
     bowl= pArgs[2]
     standard_out+="Looking for "+bowl+" bowl tickets under $"+maxPrice
     
+    connectDb()
+    if not mongo_client==None:
+        #current collection name is the url of the ticket page
+        mongo_col=mongo_db[ticket_request_url]
+
     while True:  
         df=requestSite(ticket_request_url)
         print(standard_out)
@@ -78,19 +103,28 @@ if __name__ == "__main__":
             ticket_found=False
             lowest_cost={"UpperBowl":[10000,-1],"LowerBowl":[10000,-1]}
 
+            m_requests=[]
+            now_time=datetime.now()
+            ticket_price_sum=0
+
             for i in range(len(df['Price'])):
                 price= df['Price'][i][1:df['Price'][i].find('.')]
-                lowest_cost[df['Section'][i]][0] = min(lowest_cost[df['Section'][i]][0],int(price))
-
+                ticket_price_sum += float(price)
+                lowest_cost[df['Section'][i]][0] = min(lowest_cost[df['Section'][i]][0],float(price))
+                m_requests.insert({"name":"NAME_HERE","price":float(price),"time":now_time})
                 if lowest_cost[df['Section'][i]][0]==int(price):
                     lowest_cost[df['Section'][i]][1] = i 
 
                 if df['Section'][i]==("UpperBowl" if bowl=="upper" else "LowerBowl"):
                     if int(price)<=int(maxPrice):
                         ticket_found=True
-                        #break on finding a price lower - removed for data collection
-                        #break
+
+            ticket_price_avg = ticket_price_sum / len(df['Price'])
+
             
+            #insert each ticket into that game's collection
+            mongo_db[ticket_request_url].insert_many(m_requests)
+
             print("\n\nNewest data: ")
             print("response time: "+str(r.elapsed.total_seconds()))
             print("Upper Bowl: $"+str(lowest_cost["UpperBowl"][0]) + " : "+STUDENT_SEATS_SITE+post_links[lowest_cost["UpperBowl"][1]*2])
@@ -98,7 +132,7 @@ if __name__ == "__main__":
         else:
             print("site request error")
 
-        if t_found:
+        if ticket_found:
             print('\a\a\a\a\a\a')
             print("found")
             if len(pArgs)>4:
